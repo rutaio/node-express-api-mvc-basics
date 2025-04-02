11d28ba7562e58ae0321127fdb3073525a2089ee
“rutaiogit config --global user.email youremail@gmail.comrutadanyt@gmail.com <rutadanyt@gmail.com>
2025-04-02 20:04:55 +0100

    add edits

--- frontend/src/components/CarDetails/CarDetails.tsx
+++ frontend/src/components/CarDetails/CarDetails.tsx
@@ -11,9 +11,8 @@ export const CarDetails = () => {
   // Hooks apsirasome virsuje
   const navigate = useNavigate();
   const { id } = useParams();
-  const [car, setCar] = useState<Car[]>([]);
-
-  const api = 'http://localhost:3000/api/cars';
+  // apsirasome null, tuo atveju jeigu ieskomo automobilio nebutu, tada duomenu tipas butu "null":
+  const [car, setCar] = useState<Car | null>(null);
 
   const handleBackClick = () => {
     navigate('/');
@@ -23,7 +22,9 @@ export const CarDetails = () => {
   useEffect(() => {
     const fetchCars = async () => {
       try {
-        const response = await axios.get(`${api}/${id}`);
+        const response = await axios.get(
+          `http://localhost:3000/api/cars/${id}`
+        );
         setCar(response.data);
       } catch (error) {
         console.log(error);
@@ -32,51 +33,61 @@ export const CarDetails = () => {
     fetchCars();
   }, []);
 
+  // tuo atveju jeigu ieskomo automobilio nebutu, tada rodytu error zinute:
+  if (!car) {
+    return <div>Nera tokio automobilio!</div>;
+  }
+
   return (
     <div className="car-detail">
       <div className="car-detail-container">
         {/* Kaire puse */}
         <div className="car-detail-left">
-          <img src={car.image} alt="" className="car-detail-image" />
+          <img
+            src={car.image}
+            alt="Automobilio paveikslelis"
+            className="car-detail-image"
+          />
         </div>
-
         {/* Desine puse */}
         <div className="car-detail-right">
           {/* header */}
-          <div className="car-header"></div>
-          <h2>
-            {car.make} {car.model}
-          </h2>
-          <p className="car-year">{car.year} m.</p>
-        </div>
-
-        {/* */}
-        <div className="car-description">
-          <p>Nuostabus automobilis!</p>
-        </div>
-        <div className="car-specs">
-          <div className="spec-item">
-            <span className="spec-label">Pavaru deze: </span>
-            <span className="spec-value">{car.transmission}</span>
+          <div className="car-header">
+            <h2>
+              {car.make} {car.model}
+            </h2>
+            <p className="car-year">{car.year} m.</p>
           </div>
-          <div className="spec-item">
-            <span className="spec-label">Kuro tipas: </span>
-            <span className="spec-value">{car.fuelType}</span>
+          <div className="car-description">
+            <p>Nuostabus automobilis!</p>
           </div>
-          <div className="spec-item">
-            <span className="spec-label">Sedimu vietu: </span>
-            <span className="spec-value">{car.seats}</span>
+          <div className="car-specs">
+            <div className="spec-item">
+              <span className="spec-label">Pavaru deze: </span>
+              <span className="spec-value">{car.transmission}</span>
+            </div>
+            <div className="spec-item">
+              <span className="spec-label">Kuro tipas: </span>
+              <span className="spec-value">{car.fuelType}</span>
+            </div>
+            <div className="spec-item">
+              <span className="spec-label">Sedimu vietu: </span>
+              <span className="spec-value">{car.seats}</span>
+            </div>
+            <div className="spec-item">
+              <span className="spec-label">Kaina per diena: </span>
+              <span className="spec-value">${car.price}</span>
+            </div>
           </div>
-          <div className="spec-item">
-            <span className="spec-label">Kaina per diena: </span>
-            <span className="spec-value">${car.price}</span>
+          <div className="car-actions">
+            <button className="button button-primary">Rezervuoti</button>
+            <button
+              className="button button-secondary"
+              onClick={handleBackClick}
+            >
+              Grizti i pagrindini
+            </button>
           </div>
-        </div>
-        <div className="car-actions">
-          <button className="button button-primary">Rezervuoti</button>
-          <button className="button button-secondary" onClick={handleBackClick}>
-            Grizti i pagrindini
-          </button>
         </div>
       </div>
     </div>

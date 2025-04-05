dd4ea244c421f90a0528d4cb0edbc2e5732e8214
“rutaiogit config --global user.email youremail@gmail.comrutadanyt@gmail.com <rutadanyt@gmail.com>
2025-04-05 15:45:52 +0100

    add css code for login form

--- frontend/src/components/LoginForm/LoginForm.tsx
+++ frontend/src/components/LoginForm/LoginForm.tsx
@@ -1,25 +1,34 @@
+import './login-form.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWindowClose } from '@fortawesome/free-solid-svg-icons'
+
 export const LoginForm = () => {
   return (
     <div className="login-container">
-      <div className="close-icon-container">
-        <span>x</span>
+      <div className="close-icon">
+        <FontAwesomeIcon icon={faWindowClose} />
       </div>
       <div className="form-container">
         <h2>Login Form</h2>
         <form typeof="submit">
-          <label htmlFor="username">Email or Phone</label>
+          <label className="label" htmlFor="username">
+            Email or Phone
+          </label>
           <input type="text" id="username" required />
-          <label htmlFor="">Password</label>
+          <label className="label" htmlFor="">
+            Password
+          </label>
           <input type="text" id="password" required />
-          <button id="forgot-pwd-btn">Forgot password</button>
+          <button id="forgot-pwd-btn">Forgot Password?</button>
           <button id="login-btn">Login</button>
         </form>
       </div>
-      <div className="non-users-container">
-        <span>
-          Not a member? <a href="#">Signup now.</a>
-        </span>
-      </div>
+      <p className="non-users-cta">
+        Not a member?{' '}
+        <a href="#" className="cta-link">
+          Signup now.
+        </a>
+      </p>
     </div>
   );
 };
